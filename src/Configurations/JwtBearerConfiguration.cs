7930cdbd06442ada7d9bec8e41e6d618e63c3df8
Ton Borges <ton.borges@gmail.com>
2022-06-30 14:28:16 -0300

    Exemplo de autenticação usando jwt e Keycloak

---
+++ src/Configurations/JwtBearerConfiguration.cs
@@ -0,0 +1,70 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApi.KeyCloak.Example.Configurations;
+
+public static class JwtBearerConfiguration
+{
+    public static IServiceCollection AddJwtBearerConfiguration(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                HttpClientHandler handler = new HttpClientHandler();
+                handler.ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+                options.BackchannelHttpHandler = handler;
+
+                options.Authority =
+                    $"{configuration["Keycloak:Authority"]}{configuration["Keycloak:AuthorityRealms"]}{configuration["Keycloak:Realms"]}";
+                options.Audience = configuration["Keycloak:ClientId"];
+                options.RequireHttpsMetadata = true;
+                options.IncludeErrorDetails = true;
+                options.MetadataAddress =
+                    $"{configuration["Keycloak:Authority"]}/realms/{configuration["Keycloak:Realms"]}{configuration["Keycloak:MetadataAddress"]}";
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = false,
+                    ValidateIssuer = false,
+                    ValidIssuer = $"{configuration["Keycloak:Authority"]}/realms/{configuration["Keycloak:Realms"]}",
+                    ValidateLifetime = true,
+                    SaveSigninToken = false
+                };
+
+                options.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        if (context.Exception is SecurityTokenExpiredException)
+                        {
+                            context.NoResult();
+                            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+                            return Task.CompletedTask;
+                        }
+                        else
+                        {
+                            context.NoResult();
+                            context.Response.StatusCode = context.HttpContext.Response.StatusCode;
+                            
+                            return Task.CompletedTask;
+                        }
+                    }
+                };
+            });
+
+        services.AddAuthorization();
+
+        return services;
+    }
+}
---
+++ src/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.KeyCloak.Example.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+   
+    [Authorize(Roles = "admins")]
+    [HttpGet(Name = "WeatherForecast")]
+    public IEnumerable<WeatherForecast> GetAuth()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
